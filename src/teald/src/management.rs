30d5571d0647f7b9487f8464d910f2e3a2ad44fd
Toshiyuki Igarashi <45117767+toshiyuki-igarashi@users.noreply.github.com>
2026-05-15 23:02:18 +0900

    docs: add license headers to all source files

--- src/teald/src/management.rs
+++ src/teald/src/management.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: MIT
+/*
+ * TEAL Daemon (teald)
+ *
+ * Copyright (c) 2026 Toshiyuki Igarashi
+ */
 use std::sync::{Arc, OnceLock};
 use anyhow::{Context, Result};
 use serde_json::Value;
