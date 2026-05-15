6ca126af7c040300942d7bcf808bcae62cf7ec23
Toshiyuki Igarashi <45117767+toshiyuki-igarashi@users.noreply.github.com>
2026-05-15 22:58:18 +0900

    docs: add license headers to all source files

--- src/teald/src/worker/mod.rs
+++ src/teald/src/worker/mod.rs
@@ -1,5 +1,9 @@
-// src/worker/mod.rs
-
+// SPDX-License-Identifier: MIT
+/*
+ * TEAL Daemon (teald)
+ *
+ * Copyright (c) 2026 Toshiyuki Igarashi
+ */
 pub mod admin;
 pub mod decision;
 pub mod audit;
