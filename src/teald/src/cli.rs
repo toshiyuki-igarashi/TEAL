12cc12e3fe522f354f08ccfdbb442e1dd32454cf
Toshiyuki Igarashi <45117767+toshiyuki-igarashi@users.noreply.github.com>
2026-05-15 22:59:49 +0900

    docs: add license headers to all source files

--- src/teald/src/cli.rs
+++ src/teald/src/cli.rs
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: MIT
+/*
+ * TEAL Daemon (teald)
+ *
+ * Copyright (c) 2026 Toshiyuki Igarashi
+ */
 use anyhow::{Context, Result};
 use std::env;
 use std::io::{Read, Write};
